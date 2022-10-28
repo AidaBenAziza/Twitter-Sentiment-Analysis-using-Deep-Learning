a4555df15c6c6c2862b20d761aeaca0a3eede86c
Aida Ben Aziza <aidabenaziza96@gmail.com>
2022-10-28 17:11:21 +0200

    Update Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,1 +1,250 @@
+# DataFrame
+import pandas as pd
 
+# Matplot
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+from matplotlib.ticker import MaxNLocator
+import matplotlib.gridspec as gridspec
+import matplotlib.patches as mpatches
+
+# Scikit-learn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+from sklearn.manifold import TSNE
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.decomposition import LatentDirichletAllocation, NMF
+from sklearn.metrics import f1_score, accuracy_score
+
+
+# Keras
+from keras.preprocessing.text import Tokenizer
+#from keras.preprocessing.sequence import pad_sequences
+from keras_preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Activation, Dense, Dropout, Embedding, Flatten, Conv1D, MaxPooling1D, LSTM
+from keras import utils
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping
+
+# nltk
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+from nltk.corpus import stopwords, wordnet
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+import nltk
+nltk.download('omw-1.4')
+
+# Word2vec
+import gensim
+from gensim.test.utils import common_texts
+from gensim.models import Word2Vec
+
+
+# Utility
+import string
+import re
+import numpy as np
+import os
+from collections import Counter
+import logging
+import time
+import pickle
+import itertools
+import random
+import datetime
+
+# WordCloud
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from collections import Counter, defaultdict
+
+# Warnings
+import warnings 
+warnings.filterwarnings('ignore')
+
+# Set log
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+df = pd.read_csv("C:/Users/DELL/Desktop/Projets machine learning/Sentiment Analysis using Deep Learning/Twitter_Data.csv")
+df1 = df
+df.head(5)
+df = df[df.category != 0.0]
+############ i have removed neutral comments as I wanted to perform analysis specific to Negative and Positive sentiments
+df.drop_duplicates()
+df.info()
+
+##### Data Pre-processing
+########"variable declaration"
+# Varaibles related to dataset
+DATASET_COLUMNS = ["clean_text", "category"]
+DATASET_ENCODING = "ISO-8859-1"
+TRAIN_SIZE = 0.8
+
+# Parameters for WORD2VEC
+W2V_SIZE = 300
+W2V_WINDOW = 7
+W2V_EPOCH = 32
+W2V_MIN_COUNT = 10
+
+# Parameters related to KERAS
+SEQUENCE_LENGTH = 300
+EPOCHS = 8
+BATCH_SIZE = 1024
+
+# Variable specific to SENTIMENT
+POSITIVE = "POSITIVE"
+NEGATIVE = "NEGATIVE"
+NEUTRAL = "NEUTRAL"
+SENTIMENT_THRESHOLDS = (0.4, 0.7)
+
+# Variables for Exporting purpose
+KERAS_MODEL = "model.h5"
+WORD2VEC_MODEL = "model.w2v"
+TOKENIZER_MODEL = "tokenizer.pkl"
+ENCODER_MODEL = "encoder.pkl"
+
+# MISC
+
+plt.style.use('fivethirtyeight')
+pd.options.display.max_columns = 250
+pd.options.display.max_rows = 250
+
+######## Null Values Identification and Treatment
+# Confirming presence of missing values in each variable
+df.isnull().sum()
+df.dropna(how='any', inplace=True) # Removed these rows as the count is small and will not affect overall modelling
+df.isnull().sum()
+ 
+ #####" Label Encoding"
+decode_map = {-1.0: "NEGATIVE", 1.0: "POSITIVE"}
+def decode_sentiment(label):
+    return decode_map[int(label)]
+
+df.category = df.category.apply(lambda x: decode_sentiment(x))
+category_cnt = Counter(df.category)
+
+plt.figure(figsize=(16,8))
+plt.bar(category_cnt.keys(), category_cnt.values())
+plt.title("Dataset labels distribuition")
+########### Text Manipulation and Structurization
+# Including Stopwords and exclusing 'not' related stopwords
+
+stop_words = set(stopwords.words("english"))
+stop_words.remove('not')
+more_stopwords = {'one', 'br', 'Po', 'th', 'sayi', 'fo', 'Unknown'}
+stop_words = stop_words.union(more_stopwords)
+                 
+stemmer = SnowballStemmer("english")
+def remove_URL(text):
+    url = re.compile(r'https?://\S+|www\.\S+')
+    return url.sub(r'', text)
+
+
+def remove_emoji(text):
+    emoji_pattern = re.compile(
+        '['
+        u'\U0001F600-\U0001F64F'  # emoticons
+        u'\U0001F300-\U0001F5FF'  # symbols & pictographs
+        u'\U0001F680-\U0001F6FF'  # transport & map symbols
+        u'\U0001F1E0-\U0001F1FF'  # flags (iOS)
+        u'\U00002702-\U000027B0'
+        u'\U000024C2-\U0001F251'
+        ']+',
+        flags=re.UNICODE)
+    return emoji_pattern.sub(r'', text)
+
+
+def remove_html(text):
+    html = re.compile(r'^[^ ]<.*?>|&([a-z0-9]+|#[0-9]\"\'\“{1,6}|#x[0-9a-f]{1,6});[^A-Za-z0-9]+')
+    return re.sub(html, '', text)
+
+
+def remove_punct(text):
+    table = str.maketrans('', '', string.punctuation)
+    return text.translate(table)
+
+def remove_quotes(text):
+    quotes = re.compile(r'[^A-Za-z0-9\s]+')
+    return re.sub(quotes, '', text)
+
+
+# Applying helper functions
+
+df['mod_text'] = df['clean_text'].apply(lambda x: remove_URL(x))
+df['mod_text'] = df['mod_text'].apply(lambda x: remove_emoji(x))
+df['mod_text'] = df['mod_text'].apply(lambda x: remove_html(x))
+df['mod_text'] = df['mod_text'].apply(lambda x: remove_punct(x))
+df['mod_text'] = df['mod_text'].apply(lambda x: remove_quotes(x))
+df.head()
+# Tokenizing the tweet base texts.
+
+df['tokenized'] = df['mod_text'].apply(word_tokenize)
+
+df.head()
+# Lower casing clean text.
+
+df['lower'] = df['tokenized'].apply(
+    lambda x: [word.lower() for word in x])
+
+df.head()
+# Removing stopwords.
+
+df['stopwords_removed'] = df['lower'].apply(
+    lambda x: [word for word in x if word not in stop_words])
+
+df.head()
+# Applying part of speech tags.
+
+df['pos_tags'] = df['stopwords_removed'].apply(nltk.tag.pos_tag)
+
+df.head()
+def get_wordnet_pos(tag):
+    if tag.startswith('J'):
+        return wordnet.ADJ
+    elif tag.startswith('V'):
+        return wordnet.VERB
+    elif tag.startswith('N'):
+        return wordnet.NOUN
+    elif tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
+
+
+df['wordnet_pos'] = df['pos_tags'].apply(
+    lambda x: [(word, get_wordnet_pos(pos_tag)) for (word, pos_tag) in x])
+
+df.head()
+
+# Applying word lemmatizer.
+
+wnl = WordNetLemmatizer()
+
+df['lemmatized'] = df['wordnet_pos'].apply(
+    lambda x: [wnl.lemmatize(word, tag) for word, tag in x])
+
+df['lemmatized'] = df['lemmatized'].apply(
+    lambda x: [word for word in x if word not in stop_words])
+
+df['lemma_str'] = [' '.join(map(str, l)) for l in df['lemmatized']]
+
+df.head()
+print("Total Rows =", len(df.clean_text))
+df.head()
+df_mod = df[["lemma_str", "category"]]
+df_mod.head()
+
+########### Train-Test Split
+
+df_train, df_test = train_test_split(df_mod, test_size=1-TRAIN_SIZE, random_state=42)
+print("TRAIN size:", len(df_train))
+print("TEST size:", len(df_test))
