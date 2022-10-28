6060a76e2998ff3e8a18b7f20cf9216b8ede571f
Aida Ben Aziza <aidabenaziza96@gmail.com>
2022-10-28 17:15:11 +0200

    Update Deep-Learning-Model.py

--- Deep-Learning-Model.py
+++ Deep-Learning-Model.py
@@ -1,1 +1,240 @@
+########### Sentiment Analysis using Deep-Learning Model
+# DataFrame
+import pandas as pd
+
+# Matplot
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+from matplotlib.ticker import MaxNLocator
+import matplotlib.gridspec as gridspec
+import matplotlib.patches as mpatches
+
+# Scikit-learn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+from sklearn.manifold import TSNE
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.decomposition import LatentDirichletAllocation, NMF
+from sklearn.metrics import f1_score, accuracy_score
+
+
+# Keras
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Activation, Dense, Dropout, Embedding, Flatten, Conv1D, MaxPooling1D, LSTM
+from keras import utils
+from keras.callbacks import ReduceLROnPlateau, EarlyStopping
+
+# nltk
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+from nltk.corpus import stopwords, wordnet
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+import nltk
+nltk.download('omw-1.4')
+
+# Word2vec
+import gensim
+from gensim.test.utils import common_texts
+from gensim.models import Word2Vec
+
+
+# Utility
+import string
+import re
+import numpy as np
+import os
+from collections import Counter
+import logging
+import time
+import pickle
+import itertools
+import random
+import datetime
+
+# WordCloud
+from PIL import Image
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from collections import Counter, defaultdict
+
+# Warnings
+import warnings 
+warnings.filterwarnings('ignore')
+
+# Set log
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+######### oken and Vocab Creation
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(df_train.lemma_str)
+
+vocab_size = len(tokenizer.word_index) + 1
+print("Total words", vocab_size)
+
+x_train = pad_sequences(tokenizer.texts_to_sequences(df_train.lemma_str), maxlen=SEQUENCE_LENGTH)
+x_test = pad_sequences(tokenizer.texts_to_sequences(df_test.lemma_str), maxlen=SEQUENCE_LENGTH)
+
+#######Label Encoding
+labels = df_train.category.unique().tolist()
+labels.append(NEUTRAL)
+labels
+
+encoder = LabelEncoder()
+encoder.fit(df_train.category.tolist())
+
+y_train = encoder.transform(df_train.category.tolist())
+y_test = encoder.transform(df_test.category.tolist())
+
+y_train = y_train.reshape(-1,1)
+y_test = y_test.reshape(-1,1)
+
+print("y_train",y_train.shape)
+print("y_test",y_test.shape)
+
+print("x_train", x_train.shape)
+print("y_train", y_train.shape)
+print()
+print("x_test", x_test.shape)
+print("y_test", y_test.shape)
+y_train[:10]
+
+embedding_matrix = np.zeros((vocab_size, W2V_SIZE))
+for word, i in tokenizer.word_index.items():
+  if word in w2v_model.wv:
+    embedding_matrix[i] = w2v_model.wv[word]
+print(embedding_matrix.shape)
+embedding_layer = Embedding(vocab_size, W2V_SIZE, weights=[embedding_matrix], input_length=SEQUENCE_LENGTH, trainable=False)
+
+######"""Model Creation - LSTM"
+model = Sequential()
+model.add(embedding_layer)
+model.add(Dropout(0.5))
+model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(1, activation='sigmoid'))
+
+model.summary()
+###Compiling Model
+model.compile(loss='binary_crossentropy',
+              optimizer="adam",
+              metrics=['accuracy'])
+
+###### Callback Creation
+callbacks = [ ReduceLROnPlateau(monitor='val_loss', patience=5, cooldown=0),
+              EarlyStopping(monitor='val_acc', min_delta=1e-4, patience=5)]
+####"" Model Training
+history = model.fit(x_train, y_train,
+                    batch_size=BATCH_SIZE,
+                    epochs=EPOCHS,
+                    validation_split=0.1,
+                    verbose=1,
+                    callbacks=callbacks)
+########### Model Evaluation
+score = model.evaluate(x_test, y_test, batch_size=BATCH_SIZE)
+print()
+print("ACCURACY:",score[1])
+print("LOSS:",score[0])
+
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+ 
+epochs = range(len(acc))
+ 
+plt.plot(epochs, acc, 'b', label='Training acc')
+plt.plot(epochs, val_acc, 'r', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+ 
+plt.figure()
+ 
+plt.plot(epochs, loss, 'b', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+ 
+plt.show()
+##################" Prediction using Model"
+def decode_sentiment(score, include_neutral=True):
+    if include_neutral:        
+        label = NEUTRAL ### Whatever not coming in negative and positive sentiments has been considered as neutral
+        if score <= SENTIMENT_THRESHOLDS[0]:
+            label = NEGATIVE
+        elif score >= SENTIMENT_THRESHOLDS[1]:
+            label = POSITIVE
+
+        return label
+    else:
+        return NEGATIVE if score < 0.5 else POSITIVE
+
+def predict(clean_text, include_neutral=True):
+    start_at = time.time()
+    # Tokenize clean_text
+    x_test = pad_sequences(tokenizer.texts_to_sequences([clean_text]), maxlen=SEQUENCE_LENGTH)
+    # Predict
+    score = model.predict([x_test])[0]
+    # Decode sentiment
+    label = decode_sentiment(score, include_neutral=include_neutral)
+
+    return {"label": label, "score": float(score),
+       "elapsed_time": time.time()-start_at}  
+
+predict("I love the music")
+predict("I have nothing to lose")
+predict("Experience has been bad")
+###############" Creating Confusion Matrix"
+y_pred_1d = []
+y_test_1d = list(df_test.category)
+scores = model.predict(x_test, verbose=1, batch_size=8000)
+y_pred_1d = [decode_sentiment(score, include_neutral=False) for score in scores]
+def plot_confusion_matrix(cm, classes,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title, fontsize=30)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=90, fontsize=22)
+    plt.yticks(tick_marks, classes, fontsize=22)
+
+    fmt = '.2f'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label', fontsize=25)
+    plt.xlabel('Predicted label', fontsize=25)
+
+cnf_matrix = confusion_matrix(y_test_1d, y_pred_1d)
+plt.figure(figsize=(12,12))
+plot_confusion_matrix(cnf_matrix, classes=df_train.category.unique(), title="Confusion matrix")
+plt.show()
+#############Classification Report
+print(classification_report(y_test_1d, y_pred_1d))
+ 
+ ################ Accuracy Score
+accuracy_score(y_test_1d, y_pred_1d)
+##########Saving Model for future use
+model.save(KERAS_MODEL)
+w2v_model.save(WORD2VEC_MODEL)
+pickle.dump(tokenizer, open(TOKENIZER_MODEL, "wb"), protocol=0)
+pickle.dump(encoder, open(ENCODER_MODEL, "wb"), protocol=0)
 
