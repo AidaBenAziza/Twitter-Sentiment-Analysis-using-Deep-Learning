0fc29f55bdecbcec0c36abca35269caaa7ceb8d7
Aida Ben Aziza <aidabenaziza96@gmail.com>
2022-10-28 18:12:58 +0200

    Update CNN-Deep-Learning-Model.py

--- CNN-Deep-Learning-Model.py
+++ CNN-Deep-Learning-Model.py
@@ -1,1 +1,142 @@
+###################################Machine Learning##########
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os, re, time, math, tqdm, itertools
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.offline as pyo
+import tensorflow as tf
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import classification_report, confusion_matrix
+from mlxtend.plotting import plot_confusion_matrix
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
+from sklearn.preprocessing import LabelBinarizer
+#import Dense, Dropout, Activation, Flatten, LSTM 
+from sklearn.utils import resample
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neural_network import MLPClassifier
+import keras
+from keras.layers import Conv2D, Conv1D, MaxPooling2D, MaxPooling1D, Flatten, BatchNormalization, Dense
+from keras.utils.np_utils import to_categorical
+from keras.models import Sequential
+from keras.callbacks import CSVLogger, ModelCheckpoint
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay
+from mlxtend.plotting import plot_confusion_matrix
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score
+
+
+########### Sentiment Analysis using Deep-Learning Model
+######### oken and Vocab Creation
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(df_train.lemma_str)
+
+vocab_size = len(tokenizer.word_index) + 1
+print("Total words", vocab_size)
+
+x_train = pad_sequences(tokenizer.texts_to_sequences(df_train.lemma_str), maxlen=SEQUENCE_LENGTH)
+x_test = pad_sequences(tokenizer.texts_to_sequences(df_test.lemma_str), maxlen=SEQUENCE_LENGTH)
+#######Label Encoding
+labels = df_train.category.unique().tolist()
+labels.append(NEUTRAL)
+labels
+
+encoder = LabelEncoder()
+encoder.fit(df_train.category.tolist())
+
+y_train = encoder.transform(df_train.category.tolist())
+y_test = encoder.transform(df_test.category.tolist())
+
+y_train = y_train.reshape(-1,1)
+y_test = y_test.reshape(-1,1)
+print(x_train.shape)
+print(x_test.shape)
+print(y_train.shape)
+print(y_test.shape)
+# reshape the data for CNN
+#X_train = tf.data.Dataset.from_tensor_slices((train_dataset))
+#X_test = tf.data.Dataset.from_tensor_slices((test_dataset))
+
+#X_train = np.array([np.array(val) for val in X_train])
+#X_test = np.array([np.array(val) for val in X_test])
+x_train = x_train.reshape(len(x_train), x_train.shape[1], 1)
+x_test = x_test.reshape(len(x_test), x_test.shape[1], 1)
+X_train.shape, x_test.shape
+
+# making the deep learning function
+
+def model():
+   
+    model = Sequential()
+    model.add(Conv1D(filters=124, kernel_size=6, activation='relu', 
+                    padding='same', input_shape=(1,198)))
+                    
+    model.add(BatchNormalization())
+    
+    # adding a pooling layer
+    model.add(MaxPooling1D(pool_size=(3), strides=2, padding='same'))
+    
+    model.add(Conv1D(filters=124, kernel_size=6, activation='relu', 
+                    padding='same', input_shape=(1,198)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=(3), strides=2, padding='same'))
+    
+    model.add(Conv1D(filters=124, kernel_size=6, activation='relu', 
+                    padding='same', input_shape=(1,198)))
+    model.add(BatchNormalization())
+    model.add(MaxPooling1D(pool_size=(3), strides=2, padding='same'))
+    
+    model.add(Flatten())
+    model.add(Dense(77, activation='relu'))
+    model.add(Dense(77, activation='relu'))
+    model.add(Dense(3, activation='softmax'))
+    
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+model = model()
+model.summary()
+logger = CSVLogger('logs.csv', append=True)
+his = model.fit(x_train, y_train, epochs=100, batch_size=32, 
+          validation_data=(x_test, y_test), callbacks=[logger])
+
+# check the model performance on test data
+scores = model.evaluate(x_test, y_test)
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+# check history of model
+history = his.history
+history.keys()
+epochs = range(1, len(history['loss']) + 1)
+acc = history['accuracy']
+loss = history['loss']
+val_acc = history['val_accuracy']
+val_loss = history['val_loss']
+predicted = model.predict(x_test)
+# visualize training and val accuracy
+plt.figure(figsize=(10, 5))
+plt.title('Training and Validation Accuracy (CNN)')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.plot(epochs, acc, label='accuracy')
+plt.plot(epochs, val_acc, label='val_acc')
+plt.legend()
+plt.show()
+# visualize train and val loss
+plt.figure(figsize=(10, 5))
+plt.title('Training and Validation Loss(CNN)')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.plot(epochs, loss, label='loss', color='g')
+plt.plot(epochs, val_loss, label='val_loss', color='r')
+plt.legend()
+plt.show()
+
+
 
